584ff265ad4b8859246c0be10fb33f5bdb4abb27
/kudodz/ <minhkhang.vers@gmail.com>
2025-12-20 10:42:52 +0700

    Create ptt.js

---
+++ k2kwatch/assets/ptt.js
@@ -0,0 +1,22 @@
+const loading = document.getElementById(".loading");
+const iframe = document.querySelector(".player-crop iframe");
+
+let iframeLoaded = false;
+
+iframe.addEventListener("load", () => {
+  iframeLoaded = true;
+
+  setTimeout(() => {
+    hideLoading();
+  }, 2000);
+});
+
+function hideLoading() {
+  loading.style.transition = "opacity .5s ease";
+  loading.style.opacity = "0";
+
+  setTimeout(() => {
+    loading.style.display = "none";
+    document.body.classList.remove("overflow-hidden");
+  }, 500);
+}
